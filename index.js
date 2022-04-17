b300559ce01d3c1fbba17d3b84bfe4ea758c31ad
pranavshigvan <pranavshigvan005@gmail.com>
2022-04-17 11:17:43 +0530

    added resume

--- index.js
+++ index.js
@@ -2,8 +2,24 @@ const ham = document.getElementById("ham")
 const close = document.getElementById("close")
 const nav = document.getElementById("nav")
 const header = document.getElementById("header")
+const name = document.getElementById("name")
+const email = document.getElementById("email")
+const message = document.getElementById("message")
+const form = document.getElementById("form")
+const done = document.getElementById("done")
+import {collection, addDoc ,getFirestore} from 'https://www.gstatic.com/firebasejs/9.6.10/firebase-firestore.js'
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.6.10/firebase-app.js'
 
-
+const firebaseConfig = {
+    apiKey: "AIzaSyDvpOakNW7LowGInW2FARfC0ufX53F9dDw",
+    authDomain: "my-portfolio-1e61a.firebaseapp.com",
+    projectId: "my-portfolio-1e61a",
+    storageBucket: "my-portfolio-1e61a.appspot.com",
+    messagingSenderId: "342768153765",
+    appId: "1:342768153765:web:ab402423f65d8ebd86c94c"
+  };
+const app = initializeApp(firebaseConfig)
+const db = getFirestore(app)
 ham.onclick = ()=>{
     nav.style.display = "flex";
     header.style.boxShadow = "none"
@@ -27,4 +43,20 @@ window.onresize = ()=>{
         nav.style.display = "none"
     }
     
+}
+form.onsubmit = (e)=>{
+    e.preventDefault()
+    const docRef =addDoc(collection(db, "connects"), {
+        name: name.value,
+        email: email.value,
+        message: message.value
+      }).then(res =>{
+          done.style.display = "block"
+          name.value = "";
+          email.value = "";
+          message.value = "";
+          setTimeout(()=>{
+            done.style.display = "none"
+          },2000)
+      })
 }
